8cc4f10fe7eaa6ff2ba6cc2cd73f191ffc8380cf
konyu <yusukedrive@gmail.com>
2024-10-18 16:55:51 +0900

    Update: input

--- cmd/root.go
+++ cmd/root.go
@@ -8,13 +8,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var greeting string
-var fileName string
-var supportedLanguages = []string{"ruby", "go"}
-var languageConfigMap = map[string]string{
-	"ruby": "Gemfile",
-	"go":   "go.mod",
-}
+// var greeting string
+var (
+	fileName           string
+	outputFormat       string
+	supportedLanguages = []string{"ruby", "go"}
+	languageConfigMap  = map[string]string{
+		"ruby": "Gemfile",
+		"go":   "go.mod",
+	}
+	supportedOutputFormats = map[string]bool{
+		"csv":      true,
+		"tsv":      true,
+		"markdown": true,
+	}
+)
 
 // 引数を全部設定するlintを回避
 //
@@ -31,22 +39,35 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(_ *cobra.Command, args []string) {
 		if len(args) == 0 {
-			fmt.Println("Please Enter specify a language (" + strings.Join(supportedLanguages, " or ") + ")")
+			fmt.Println("Please Enter specify a language (" +
+				strings.Join(supportedLanguages, " or ") + ")")
 			os.Exit(1)
 		}
 
 		language := args[0] // Get the language argument
 		if !isSupportedLanguage(language) {
-			fmt.Println("Error: Unsupported language:", language)
+			fmt.Printf("Error: Unsupported language: %s. Supported languages are: %s\n",
+				language, strings.Join(supportedLanguages, ", "))
 			os.Exit(1)
 		}
 
 		if fileName == "" {
 			fileName = languageConfigMap[language]
 		}
-		fmt.Println("Reading file:", fileName)
 
-		fmt.Println(greeting, "World!")
+		if !supportedOutputFormats[outputFormat] {
+			var keys []string
+			for key := range supportedOutputFormats {
+				keys = append(keys, key)
+			}
+			fmt.Printf("Error: Unsupported output format: %s. Supported output formats are: %s\n",
+				outputFormat, strings.Join(keys, ", "))
+			os.Exit(1)
+		}
+
+		fmt.Println("Language", language)
+		fmt.Println("Reading file:", fileName)
+		fmt.Println("Output format:", outputFormat)
 	},
 }
 
@@ -68,7 +89,7 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&fileName, "file", "f", "", "Specify the file to read")
+	rootCmd.Flags().StringVarP(&fileName, "input", "i", "", "Specify the file to read")
+	rootCmd.Flags().StringVarP(&outputFormat, "format", "f", "markdown", "Specify the output format (csv, tsv, markdown)")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.Flags().StringVarP(&greeting, "greeting", "g", "Hello", "Greeting message to display before 'World!'")
 }
