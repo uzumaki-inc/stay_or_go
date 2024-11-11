54a059d8f3969ed1c7cfd149f461cbb3508ea9a8
konyu <yusukedrive@gmail.com>
2024-11-11 12:43:46 +0900

    Add: CLI description

--- cmd/root.go
+++ cmd/root.go
@@ -38,13 +38,10 @@ var (
 var rootCmd = &cobra.Command{
 	Use:     "StayOrGo",
 	Version: "0.1.0",
-	Short:   "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short:   "Analyze and score your Go and Ruby dependencies for popularity and maintenance",
+	Long: `StayOrGo scans your Go (go.mod) and Ruby (Gemfile) dependency files to evaluate each library's popularity and maintenance status.
+It generates scores to help you decide whether to keep (‘Stay’) or replace (‘Go’) your dependencies.
+Output the results in Markdown, CSV, or TSV formats.`,
 	Run: func(_ *cobra.Command, args []string) {
 
 		if len(args) == 0 {
